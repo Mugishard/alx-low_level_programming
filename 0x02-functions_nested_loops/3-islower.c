bca9381f48a8360807f4fbe2842a52ef4ec67be8
Richard Mugisha <richard.mugisha@acity.edu.gh>
2022-09-25 13:02:39 -0700

    int _islower(int c);

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _islower -> checks if the character is lowercase
+ * REturn: returns 1 ansd 0 depending on condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z')
+}
