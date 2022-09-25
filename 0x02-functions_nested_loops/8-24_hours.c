5efe1eec59d360ce283d252aac680c619b1a9e4e
Richard Mugisha <richard.mugisha@acity.edu.gh>
2022-09-25 15:35:26 -0700

    m

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,7 +21,7 @@ void jack_bauer(void)
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
 			}
-			if (j < 10)
+			if (j <= 10)
 			{
 				_putchar(':');
 				_putchar('0');
@@ -33,7 +33,7 @@ void jack_bauer(void)
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
